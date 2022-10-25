6031cc23926a31feac3ea8c31c7afe13b25be302
Graphley <graphleyfrank058@gmail.com>
2022-10-24 22:44:10 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints all the numbers of base 16 in lowercase.
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-char c;
+int num;
+char letter;
 
-int d;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
 
-c = 'a';
-d = 0;
-while
-(d < 10)
-{
-putchar(d + '0');
-d++;
-}
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
 
-while
-
-(c <= 'f');
-{
-putchar(c);
-c++;
-}
 putchar('\n');
+
 return (0);
 }
